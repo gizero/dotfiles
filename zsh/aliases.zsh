2bc0145ffe7917a1eec5137052dfa0485a35e252
Andrea Galbusera <gizero@gmail.com>
2015-02-16 12:28:49 +0100

    zsh: disable alias for less
    * due to different options between linux and osx versions of less

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -104,7 +104,7 @@ alias gbg='git bisect good'
 alias gbb='git bisect bad'
 
 # Common shell functions
-alias less='less -r'
+#alias less='less -r'
 alias tf='tail -f'
 alias l='less'
 alias lh='ls -alt | head' # see the last modified files
